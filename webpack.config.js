3ea38edf974e23d5bf121aa1a4aaa3be07428380
e <619958091@qq.com>
2018-11-28 14:28:46 +0800

    提交eslint格式修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const webpack = require('webpack');
-const path = require('path');
-const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack')
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   entry: ['webpack/hot/poll?1000', './src/main.hmr.ts'],
@@ -8,27 +8,27 @@ module.exports = {
   target: 'node',
   externals: [
     nodeExternals({
-      whitelist: ['webpack/hot/poll?1000'],
-    }),
+      whitelist: ['webpack/hot/poll?1000']
+    })
   ],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
+        exclude: /node_modules/
+      }
+    ]
   },
-  mode: "development",
+  mode: 'development',
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js']
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'server.js',
-  },
-};
+    filename: 'server.js'
+  }
+}
