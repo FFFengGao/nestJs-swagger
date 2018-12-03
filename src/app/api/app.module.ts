54ca11987fd6bc859c57ab3a0ec8934a88685ed8
e <619958091@qq.com>
2018-12-03 17:24:46 +0800

    提交orm配置

--- src/app/api/app.module.ts
+++ src/app/api/app.module.ts
@@ -4,18 +4,7 @@ import { TestModule } from './index/app.module';
 import { UserModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [DogsModule, TestModule, UserModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '1qaz@WSX',
-      database: 'nestJs',
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-  ],
+  imports: [TypeOrmModule.forRoot(), DogsModule, TestModule, UserModule],
   exports: [AppModule],
 })
 export class AppModule {}
