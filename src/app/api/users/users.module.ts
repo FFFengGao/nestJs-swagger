418e7ca16464332cb7b4f829de04f267930d6508
e <619958091@qq.com>
2018-12-05 16:39:53 +0800

    提交数据库配置目录

--- src/app/api/users/users.module.ts
+++ src/app/api/users/users.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { UsersEntity } from './user.entity';
+import { UsersEntity } from '../../dataBase/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UsersEntity])],
--- src/app/api/users/users.service.ts
+++ src/app/api/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { UsersEntity } from './user.entity';
+import { UsersEntity } from '../../dataBase/user.entity';
 
 @Injectable()
 export class UsersService {
